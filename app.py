d41097d64abae9f03ed253df6403a11d344f37e9
Paschal <blaizepaschal@gmail.com>
2021-05-23 12:32:56 +0100

    Done

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ async def index():
     return response
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True) #reload can be turned off
+    uvicorn.run("app:app", host="127.0.0.1", port=8000)#, reload=True) #reload can be turned on
