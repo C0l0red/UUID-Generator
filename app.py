360c5fb771abab311825c913aae5d88c01f16d1b
Paschal <blaizepaschal@gmail.com>
2021-05-23 11:29:58 +0100

    Added unctionality

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 from redis import StrictRedis
-# from typing import Optional
+from uuid import uuid4
+from datetime import datetime
 
 app = FastAPI()
 
-r = StrictRedis()
+r = StrictRedis(decode_responses=True)
 
 @app.get("/uuid")
 async def index():
+    date = str(datetime.now())
+    uuid = str(uuid4())
 
-    return "Hello world"
+    r.hset("uuids", date, uuid)
+    uuids = r.hgetall("uuids")
+
+    response = dict(reversed(uuids.items()))
+
+    return response
